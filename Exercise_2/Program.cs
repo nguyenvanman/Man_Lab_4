58e1dd8ec62a68964382a3ad246d6efe0843fede
Mẫn <vanman.lqd@gmail.com>
2017-11-16 22:25:46 +0700

    Add Exercise 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExtensions
+{
+    public static class StringExtension
+    {
+        public static string CaptionFirst(this String str)
+        {
+            if (str.Length == 0) return str;
+            string captionFirstString = str[0].ToString().ToUpper();
+            for (int i = 1; i < str.Length; i++)
+            {
+                captionFirstString += str[i].ToString().ToLower();
+            }
+            return captionFirstString;
+        }
+    }
+}
+namespace Exercise_2
+{
+    using CustomExtensions;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "heLlO";
+            Console.WriteLine(str.CaptionFirst());
+            Console.ReadKey();
+        }
+    }
+}
