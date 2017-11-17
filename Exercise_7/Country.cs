6446f237ca797f4483fec566bad9e7aba805ae0e
Mẫn <vanman.lqd@gmail.com>
2017-11-17 20:34:12 +0700

    Add Exercise 7

---
+++ Exercise_7/Country.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    public class Country : ICompare
+    {
+        public string Name { get; set; }
+        public double Area { get; set; }
+
+        public Country(string name, double area)
+        {
+            Name = name;
+            Area = area;
+        }
+
+        bool ICompare.IsBigger(ICompare another)
+        {
+            return Area > ((Country) another).Area;
+        }
+
+        public override string ToString()
+        {
+            return $"Country: {Name}\tArea: {Area}";
+        }
+    }
+}
---
+++ Exercise_7/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    public class Student : ICompare
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Score { get; set; }
+
+        public Student(string name, DateTime birthday, double score)
+        {
+            Name = name;
+            Birthday = birthday;
+            Score = score;
+        }
+
+        bool ICompare.IsBigger(ICompare another)
+        {
+            return Score > ((Student) another).Score;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\tBirthday: "
+                   + Birthday.ToString("yyyy-M-d dddd")
+                   + "\tScore: " + Score;
+        }
+    }
+}
